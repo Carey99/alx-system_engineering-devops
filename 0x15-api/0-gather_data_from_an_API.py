46c7f6a333764cc8f553cc218fdcf807b93f188d
Carey99 <edwinakidah1@gmail.com>
2024-05-03 22:43:11 -0700

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Gathering todo list of user"""
+import requests
+import sys
+
+
+def todo_list(emp_id):
+    url = "https://jsonplaceholder.typicode.com"
+    user_url = f'{url}/users/{emp_id}'
+    todo_url = f'{url}/todos?userId={emp_id}'
+
+    emp_data = requests.get(user_url).json()
+    name = emp_data["name"]
+
+    todo_data = requests.get(todo_url).json()
+    total = len(todo_data)
+
+    completed = 0
+    for todo in todo_data:
+        if todo["completed"] is True:
+            completed += 1
+    print(f'Employee {name} is done with tasks({completed}/{total})')
+
+    for todo in todo_data:
+        if todo["completed"] is True:
+            print(f'\t {todo["title"]}')
+
+
+if __name__ == "__main__":
+    todo_list(int(sys.argv[1]))
