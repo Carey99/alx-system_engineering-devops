617c4ed4b2777238a8b4516d0726d6527e2b092d
Carey99 <edwinakidah1@gmail.com>
2024-05-04 09:11:01 -0700

    Script to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Exporting script to CSV"""
+import requests
+import csv
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    user = f'{url}/users/{sys.argv[1]}'
+    todo_url = f'{url}/todos?userId={sys.argv[1]}'
+
+    user_data = requests.get(user).json()
+    todos_data = requests.get(todo_url).json()
+
+    name = user_data["username"]
+    user_id = sys.argv[1]
+
+    with open("{}.csv".format(user_id), mode="w") as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+
+        for todo in todos_data:
+            csv_writer.writerow([todo['userId'],
+                                name,
+                                todo['completed'],
+                                todo['title']])
