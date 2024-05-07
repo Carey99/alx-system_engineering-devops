ac10342e0b702dfb0c58ee86bb01fdc098ce9c74
Carey99 <edwinakidah1@gmail.com>
2024-05-07 09:24:30 -0700

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,8 +5,9 @@ import requests
 
 def top_ten(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
-    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
-
+    headers = {
+            'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'
+    }
     params = {'limit': 10}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
